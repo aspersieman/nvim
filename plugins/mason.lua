fe92e57fa72bd10a0a447667e3bc8ef66c003643
Nicol van der Merwe <nicol@evsolutions.biz>
2023-04-02 14:34:06 +0200

    New user config for AstroNvim

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,18 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "phpactor", "cssls", "dockerls", "eslint", "gopls", "html", "tsserver", "sqlls", "volar" },
+      ensure_installed = {
+        "lua_ls",
+        "phpactor",
+        "cssls",
+        "dockerls",
+        "eslint",
+        "gopls",
+        "html",
+        "tsserver",
+        "sqlls",
+        "volar",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
