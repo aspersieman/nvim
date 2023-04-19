6177cddc072dc03e3877992c965ed44dc5eb8ebd
Nicol van der Merwe <nicol@evsolutions.biz>
2023-04-19 09:57:58 +0200

    Added nvim plugin: todo-comments

---
+++ plugins/todo-comments.lua
@@ -0,0 +1,12 @@
+-- TODO:  TEST
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function() require("todo-comments").setup {} end,
+  opts = {},
+  event = "User AstroFile",
+  cmd = { "TodoQuickFix" },
+  keys = {
+    { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+  },
+}
