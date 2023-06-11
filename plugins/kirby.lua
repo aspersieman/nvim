665998c1df8529ee40bddf0bc0f5c2a36b6ace54
Nicol van der Merwe <nicol@evsolutions.biz>
2023-06-11 11:25:37 +0200

    Added kirby plugin

---
+++ plugins/kirby.lua
@@ -0,0 +1,9 @@
+return {
+  "romgrk/kirby.nvim",
+  dependencies = {
+    { "romgrk/fzy-lua-native",      build = "make install" },
+    { "romgrk/kui.nvim" },
+    { "nvim-tree/nvim-web-devicons" },
+  },
+  lazy = false,
+}
