29c759da8cd6800cabb580e392332e46d25935cf
Nicol van der Merwe <nicol@evsolutions.biz>
2024-03-14 13:06:23 +0200

    Added nvim-silicon plugin

---
+++ plugins/nvim-silicon.lua
@@ -0,0 +1,11 @@
+return {
+  "michaelrommel/nvim-silicon",
+  lazy = true,
+  cmd = "Silicon",
+  config = function()
+    require("silicon").setup {
+      -- Configuration here, or leave empty to use defaults
+      font = "UbuntuMono Nerd Font",
+    }
+  end,
+}
