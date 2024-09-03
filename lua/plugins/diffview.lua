08bb475719591ec85823a4dd0e685ce445e8d8bc
Nicol van der Merwe <nicol@jvrafrica.co.za>
2024-09-03 08:36:41 +0200

    New plugin with keymaps: Diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      {
+        "<leader>gd",
+        desc = "Diffview",
+      },
+      {
+        "<leader>gdo",
+        "<cmd>:DiffviewOpen<cr>",
+        desc = "Open Diffview",
+      },
+      {
+        "<leader>gdc",
+        "<cmd>:DiffviewClose<cr>",
+        desc = "Close Diffview",
+      },
+      {
+        "<leader>gdf",
+        "<cmd>:DiffviewFileHistory<cr>",
+        desc = "File History",
+      },
+    },
+  },
+}
