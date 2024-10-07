059f3e2ef8ca88daa94e9c92db0869e82bb3a0cb
Nicol van der Merwe <nicol@jvrafrica.co.za>
2024-10-07 13:28:56 +0200

    Added menu.lua plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,17 @@ vim.keymap.set("n", "<C-j>", "<cmd>%!jq .<CR>", opts)
 
 -- Visual-mode commands
 -- vim.keymap.set("v", "<A-l>", ":MoveHBlock(1)<CR>", opts)
+
+-- menu
+-- Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open("default")
+end, {})
+
+-- mouse users + nvimtree users!
+vim.keymap.set("n", "<RightMouse>", function()
+  vim.cmd.exec('"normal! \\<RightMouse>"')
+
+  local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
+  require("menu").open(options, { mouse = true })
+end, {})
