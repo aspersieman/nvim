2c717489cf3c14e3f67f2bf0cbfd93d08ba992f4
Nicol van der Merwe <nicol@jvrafrica.co.za>
2024-10-10 08:32:35 +0200

    dadbod-ui keymaps; removed menu

--- lua/plugins/dadbod-ui.lua
+++ lua/plugins/dadbod-ui.lua
@@ -4,4 +4,20 @@ return {
   config = function() end,
   event = { "UIEnter User AstroFile" },
   lazy = false,
+  keys = {
+    {
+      "<leader>D",
+      desc = "Dadbod",
+    },
+    {
+      "<leader>Du",
+      "<cmd>:DBUI<cr>",
+      desc = "UI",
+    },
+    {
+      "<leader>Da",
+      "<cmd>:DBUIAddConnection<cr>",
+      desc = "Add connection",
+    },
+  },
 }
