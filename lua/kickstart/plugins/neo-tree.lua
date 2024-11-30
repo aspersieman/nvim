7254723f0b03d428939edadddda2fca81d64531b
Nicol van der Merwe <nicol@evsolutions.biz>
2024-11-30 20:24:11 +0200

    Neotree source selector as tabs

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,10 +14,88 @@ return {
     { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
   },
   opts = {
+    sources = { 'filesystem', 'buffers', 'git_status' },
+    source_selector = {
+      winbar = true, -- toggle to show selector on winbar
+      statusline = false, -- toggle to show selector on statusline
+      show_scrolled_off_parent_node = false, -- this will replace the tabs with the parent path
+      -- of the top visible node when scrolled down.
+      sources = {
+        { source = 'filesystem' },
+        { source = 'buffers' },
+        { source = 'git_status' },
+      },
+      content_layout = 'start', -- only with `tabs_layout` = "equal", "focus"
+      --                start  : |/ 󰓩 bufname     \/...
+      --                end    : |/     󰓩 bufname \/...
+      --                center : |/   󰓩 bufname   \/...
+      tabs_layout = 'equal', -- start, end, center, equal, focus
+      --             start  : |/  a  \/  b  \/  c  \            |
+      --             end    : |            /  a  \/  b  \/  c  \|
+      --             center : |      /  a  \/  b  \/  c  \      |
+      --             equal  : |/    a    \/    b    \/    c    \|
+      --             active : |/  focused tab    \/  b  \/  c  \|
+      truncation_character = '…', -- character to use when truncating the tab label
+      tabs_min_width = nil, -- nil | int: if int padding is added based on `content_layout`
+      tabs_max_width = nil, -- this will truncate text even if `text_trunc_to_fit = false`
+      padding = 0, -- can be int or table
+      -- padding = { left = 2, right = 0 },
+      -- separator = "▕", -- can be string or table, see below
+      separator = { left = '▏', right = '▕' },
+      -- separator = { left = "/", right = "\\", override = nil },     -- |/  a  \/  b  \/  c  \...
+      -- separator = { left = "/", right = "\\", override = "right" }, -- |/  a  \  b  \  c  \...
+      -- separator = { left = "/", right = "\\", override = "left" },  -- |/  a  /  b  /  c  /...
+      -- separator = { left = "/", right = "\\", override = "active" },-- |/  a  / b:active \  c  \...
+      -- separator = "|",                                              -- ||  a  |  b  |  c  |...
+      separator_active = nil, -- set separators around the active tab. nil falls back to `source_selector.separator`
+      show_separator_on_edge = false,
+      --                       true  : |/    a    \/    b    \/    c    \|
+      --                       false : |     a    \/    b    \/    c     |
+      highlight_tab = 'NeoTreeTabInactive',
+      highlight_tab_active = 'NeoTreeTabActive',
+      highlight_background = 'NeoTreeTabInactive',
+      highlight_separator = 'NeoTreeTabSeparatorInactive',
+      highlight_separator_active = 'NeoTreeTabSeparatorActive',
+    },
+    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
     filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ['l'] = 'open',
+        ['h'] = 'close_node',
+        ['<space>'] = 'none',
+        ['Y'] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg('+', path, 'c')
+          end,
+          desc = 'Copy Path to Clipboard',
+        },
+        ['O'] = {
+          function(state)
+            require('lazy.util').open(state.tree:get_node().path, { system = true })
+          end,
+          desc = 'Open with System Application',
+        },
+        ['P'] = { 'toggle_preview', config = { use_float = false } },
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = '',
+        expander_expanded = '',
+        expander_highlight = 'NeoTreeExpander',
+      },
+      git_status = {
+        symbols = {
+          unstaged = '󰄱',
+          staged = '󰱒',
         },
       },
     },
