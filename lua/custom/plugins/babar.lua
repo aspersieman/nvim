99f0c4efa7f3de5abcdffb7106cb636aae49f2a1
Nicol van der Merwe <nicol@evsolutions.biz>
2024-11-30 20:33:37 +0200

    Removed bufferline; Added babar

---
+++ lua/custom/plugins/babar.lua
@@ -0,0 +1,23 @@
+return {
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  },
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
+  opts = {
+    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    -- animation = true,
+    -- insert_at_start = true,
+    -- …etc.
+  },
+  version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  keys = {
+    { '[b', '<cmd>BufferPrevious<cr>', desc = 'Prev Buffer' },
+    { ']b', '<cmd>BufferNext<cr>', desc = 'Next Buffer' },
+    { '[B', '<cmd>BufferMovePrevious<cr>', desc = 'Move buffer prev' },
+    { ']B', '<cmd>BufferMoveNext<cr>', desc = 'Move buffer next' },
+  },
+}
