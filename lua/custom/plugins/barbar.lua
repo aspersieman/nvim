6a3761c985fd4f736f76c2e3e3b045998f42cc8f
Nicol van der Merwe <nicol@evsolutions.biz>
2024-12-02 07:37:54 +0200

    Typo

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -5,14 +5,9 @@ return {
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
   init = function()
-    vim.g.barbar_auto_setup = false
+    vim.g.barbar_auto_setup = true
+    require('barbar').setup()
   end,
-  opts = {
-    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-    -- animation = true,
-    -- insert_at_start = true,
-    -- …etc.
-  },
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
   keys = {
     { '[b', '<cmd>BufferPrevious<cr>', desc = 'Prev Buffer' },
