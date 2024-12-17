0d69634d64786a8502ba2693dc73cb92b0d4ace8
Nicol van der Merwe <nicol@jvrafrica.co.za>
2024-12-17 15:51:48 +0200

    Show recording macro status lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,7 +1,13 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'yavorski/lualine-macro-recording.nvim' },
   init = function()
     require('lualine').setup(require('lualine').get_config())
   end,
+  opts = {
+    sections = {
+      -- add to section of your choice
+      lualine_c = { 'macro_recording', '%S' },
+    },
+  },
 }
