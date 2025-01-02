ed5e7e183782a6ad732ac7edf76fd66ec980edc7
Nicol van der Merwe <nicol@evsolutions.biz>
2025-01-02 14:35:10 +0200

    New plugin: dadbod-ui

---
+++ lua/custom/plugins/dadbod-ui.lua
@@ -0,0 +1,23 @@
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = { 'tpope/vim-dadbod', 'tpope/vim-dotenv' },
+  config = function() end,
+  event = { 'UIEnter User AstroFile' },
+  lazy = false,
+  keys = {
+    {
+      '<leader>D',
+      desc = 'Dadbod',
+    },
+    {
+      '<leader>Du',
+      '<cmd>:DBUI<cr>',
+      desc = 'UI',
+    },
+    {
+      '<leader>Da',
+      '<cmd>:DBUIAddConnection<cr>',
+      desc = 'Add connection',
+    },
+  },
+}
