66414f8eee0f5d6eea258e185043a8c90324b9bd
Nicol van der Merwe <nicol@evsolutions.biz>
2025-01-09 15:26:53 +0200

    Accidental deletion

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -6,6 +6,7 @@ return {
   },
   init = function()
     vim.g.barbar_auto_setup = true
+    require('barbar').setup()
   end,
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
   keys = {
